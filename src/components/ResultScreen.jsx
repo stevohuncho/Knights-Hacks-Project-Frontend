7abad70b1d189369636e3fbefd3f3bfdc0c1110c
stevo <93010314+stevohuncho@users.noreply.github.com>
2023-04-02 03:26:53 -0400

    added backend interaction for results page

--- src/components/ResultScreen.jsx
+++ src/components/ResultScreen.jsx
@@ -2,13 +2,97 @@ import { Text, View, } from 'react-native';
 import { myStyles } from '../styling/GlobalStyles';
 import { Button } from './Button';
 import { Result } from './Result';
+import { useEffect, useState } from 'react';
 
+/*
+    Task 1: 
+        - change line 27 (fetch request) to be formated with the users coordinates
+            - ex: `http://54.164.138.55/nearme?lat=${userLat}&long=${userLong}`
+    Task 2: 
+        - fix the css with the results components
+            - add text overflow handling 
+            - add scrolling for when list is too long
+    Task 3: 
+        - work on formating restaurant data in Result component
+        - current restuarant data from request
+            - ex:   
+                "4 Rivers Smokehouse": {
+                    "address": "11764 University Boulevard, Orlando",
+                    "id": "ChIJbzG0T41o54gRfTWeGQ6o7Ew",
+                    "keywords": [
+                        "restaurant",
+                        "food",
+                        "point_of_interest",
+                        "store",
+                        "establishment"
+                    ],
+                    "num_of_ratings": 2564,
+                    "price_level": 2,
+                    "rating": 4.5
+                }
+            - id is used with http://54.164.138.55/restaurant?id=${restuarantId}
+                - this returns more detailed data for the restaurant like hours of operation etc.
+            - price level represents how many $'s
+            - doesn't give distance away but rather puts address
+                - NOT PRIORITY
+                - find way to calcualte distance using our location
+                - ill find a way to add it to the response so u dont have to (this is why not priority)
+*/
 
 export const ResultScreen = ({ navigation }) => {
+    const [results, setResults] = useState({});
+    const [nextPageToken, setNextPageToken] = useState('');
+
+    // hook runs on initialization
+    useEffect(() => {
+        async function getResults() {
+            var returnData = {
+                success: false,
+                data: {},
+                error: ""
+            };
+
+            // send request to api
+            try {
+                var resp = await fetch("http://54.164.138.55/nearme?lat=28.5970378&long=-81.2276083");
+                // check for success status code
+                if (resp.status != 200) {
+                    returnData.error = `failed to send results request. status code: ${resp.status}`;
+                    return returnData;
+                }
+                // get response json
+                returnData.success = true;
+                returnData.data = await resp.json();
+                // save next page token
+                setNextPageToken(returnData.data['next_page_token']);
+                // remove next_page_token key to prevent it from creating Result component with tis data
+                delete returnData.data['next_page_token'];
+                console.log(`successful results request.`);
+                return returnData;
+            } catch (e) {
+                // throw err
+                returnData.error = `failed to send results request. ERROR: ${String(e)}`;
+                return returnData;
+            };
+        }
+
+        getResults().then((res) => {
+            // skip if failed
+            if (!res.success) {
+                console.log(res.error)
+                return
+            }
+            // set results data
+            setResults(res.data)
+        })
+    }, []);
+
     return (
         <View style={myStyles.screen}>
             <Text style={myStyles.text}>Result Screen</Text>
-            <Result Restaurant={"Chilli's"} Distance={"13"} Price={"$$"} />
+            {Object.entries(results).map(([key, value]) => ( /* creates Result component for each restuarant in results  */
+                <Result Restaurant={key} Distance={value['address']} Price={String(value['price_level'])} />
+            ))}
             <View style={myStyles.buttonRow}>
                 <Button title="Go back" onPress={() => navigation.goBack()} />
                 <Button title="Spin the Wheel!" onPress={() => navigation.navigate('Wheel')} />
